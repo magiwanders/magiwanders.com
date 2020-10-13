f78e3407a5ed6195935f7a060c375e075af31aa5
magiwanders <magi.wanders@gmail.com>
2020-10-13 18:43:17 +0200

    migration

--- learning/HTML_CSS_JS/W2E1_DrumMachine/js.js
+++
@@ -1,8 +0,0 @@
-let instrument = document.querySelector('.name');
-
-instrument.onclick = toggleInstrument;
-
-function toggleInstrument() {
- const k = document.getElementById("kick")
- k.firstChildElement.classList.toggle("on");
-}
